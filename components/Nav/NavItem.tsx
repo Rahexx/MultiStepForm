b95164821318b1d27ddd1307b602c9df7cd6e719
Rahexx <endriu1297@gmail.com>
2026-01-20 13:51:06 +0100

    styles: add active state for NavItem component

--- components/Nav/NavItem.tsx
+++ components/Nav/NavItem.tsx
@@ -1,6 +1,14 @@
+import clsx from "clsx";
+
 function NavItem({stepNumber}: {stepNumber: number}) {
     return (
-        <li className="h-[2.0625rem] w-[2.0625rem] flex items-center justify-center border border-white text-white rounded-full hover:cursor-pointer">
+        <li className={clsx(
+            "h-[2.0625rem] w-[2.0625rem] flex items-center justify-center border rounded-full text-sm font-bold tracking-[0.063rem] hover:cursor-pointer",
+            {
+                "bg-blue-200 border-blue-200 text-black": stepNumber === 1,
+                "border-white text-white": stepNumber !== 1
+            }
+        )}>
             {stepNumber}
         </li>
     )

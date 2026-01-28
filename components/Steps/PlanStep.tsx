d3ba05a3dd91753c99964b9e1d7e98a9128cf930
Rahexx <endriu1297@gmail.com>
2026-01-28 20:35:19 +0100

    feat: refactor PlanStep to use PLANS constant for dynamic plan options

--- components/Steps/PlanStep.tsx
+++ components/Steps/PlanStep.tsx
@@ -1,35 +1,23 @@
 import PlanList from "../Form/PlanList";
 import PlanOption from "../Form/PlanOption";
 import PlanLayout from "../Layout/PlanLayout";
+import {PLANS} from "@/config/plans.config";
 
 function PlanStep() {
     return (
         <PlanLayout>
             <PlanList>
-                <PlanOption 
-                    icon="icon-arcade.svg" 
-                    optionName="Arcade" 
-                    isActive={false} 
-                    isYearlyCharge={false} 
-                    monthlyCharge={9} 
-                    yearlyCharge={90} 
-                />
-                <PlanOption 
-                    icon="icon-advanced.svg" 
-                    optionName="Advanced" 
-                    isActive={false} 
-                    isYearlyCharge={false} 
-                    monthlyCharge={12} 
-                    yearlyCharge={120} 
-                />
-                <PlanOption 
-                    icon="icon-pro.svg" 
-                    optionName="Pro" 
-                    isActive={false} 
-                    isYearlyCharge={false} 
-                    monthlyCharge={15} 
-                    yearlyCharge={150} 
-                />
+                {PLANS.map(plan => (
+                    <PlanOption 
+                        key={plan.id}
+                        icon={plan.icon}
+                        optionName={plan.optionName}
+                        isActive={false}
+                        isYearlyCharge={false}
+                        monthlyCharge={plan.monthlyCharge}
+                        yearlyCharge={plan.yearlyCharge}
+                    />
+                ))}
             </PlanList>
         </PlanLayout>
     );
---
+++ config/plans.config.ts
@@ -0,0 +1,23 @@
+export const PLANS = [
+  {
+    id: 1,
+    icon: "icon-arcade.svg",
+    optionName: "Arcade",
+    monthlyCharge: 9,
+    yearlyCharge: 90,
+  },
+  {
+    id: 2,
+    icon: "icon-advanced.svg",
+    optionName: "Advanced",
+    monthlyCharge: 12,
+    yearlyCharge: 120,
+  },
+  {
+    id: 3,
+    icon: "icon-pro.svg",
+    optionName: "Pro",
+    monthlyCharge: 15,
+    yearlyCharge: 150,
+  },
+] as const;

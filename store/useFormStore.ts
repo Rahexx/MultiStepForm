ad55b9176e07d42654298aa4cc6c341773e39ee6
Rahexx <endriu1297@gmail.com>
2026-01-29 17:16:32 +0100

    feat: extend store for new form values: activePlan and isYearlyCharge.

--- store/useFormStore.ts
+++ store/useFormStore.ts
@@ -5,10 +5,14 @@ interface FormState {
     email: string;
     phoneNumber: string;
     errors: { [key: string]: boolean };
+    activePlan: string;
+    isYearlyCharge: boolean;
     setName: (name: string) => void;
     setEmail: (email: string) => void;
     setPhoneNumber: (phoneNumber: string) => void;
     setErrors: (errors: { [key: string]: boolean }) => void;
+    setActivePlan: (activePlan: string) => void;
+    setIsYearlyCharge: (isYearlyCharge: boolean) => void;
 }
 
 export const useFormStore = create<FormState>((set) => ({
@@ -16,8 +20,12 @@ export const useFormStore = create<FormState>((set) => ({
     email: '',
     phoneNumber: '',
     errors: {},
+    activePlan: 'Arcade',
+    isYearlyCharge: false,
     setName: (name) => set({ name }),
     setEmail: (email) => set({ email }),
     setPhoneNumber: (phoneNumber) => set({ phoneNumber }),
     setErrors: (errors) => set({ errors }),
+    setActivePlan: (activePlan) => set({ activePlan}),
+    setIsYearlyCharge: (isYearlyCharge) => set({ isYearlyCharge }),
 }));

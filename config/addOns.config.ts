521c97c90746ef87f06d7c3ae1362178dc685bb7
Rahexx <endriu1297@gmail.com>
2026-01-30 16:22:40 +0100

    feat: create config for addOns

---
+++ config/addOns.config.ts
@@ -0,0 +1,23 @@
+export const ADD_ONS = [
+    {
+        id: 1,
+        title: "Online service",
+        description: "Access to multiplayer games",
+        monthlyPrice: 1,
+        yearlyPrice: 10,
+    },
+    {
+        id: 2,
+        title: "Larger storage",
+        description: "Extra 1TB of cloud save",
+        monthlyPrice: 2,
+        yearlyPrice: 20,
+    },
+    {
+        id: 3,
+        title: "Customizable profile",
+        description: "Custom theme on your profile",
+        monthlyPrice: 2,
+        yearlyPrice: 20,
+    },
+] as const;

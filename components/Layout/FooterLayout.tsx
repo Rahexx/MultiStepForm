5af3e7a33cc2a7c255b6b60e441fa81fe81181f5
Rahexx <endriu1297@gmail.com>
2026-03-02 18:18:35 +0100

    feat: add tablet styles for FormLayout component

--- components/Layout/FooterLayout.tsx
+++ components/Layout/FooterLayout.tsx
@@ -1,6 +1,6 @@
 function FooterLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="h-[4.5rem] flex items-center justify-center bg-white md:absolute md:bottom-8 md:right-[3.688rem]">
+        <div className="h-[4.5rem] flex items-center justify-center bg-white md:absolute md:bottom-8 md:right-[3.688rem] md:bg-transparent">
             <div className="w-[21.438rem] flex items-center justify-between md:w-[21.75rem]">
                 {children}
             </div>

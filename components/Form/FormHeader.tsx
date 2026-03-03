f57604fb8b0ed34f2687f5f0f7dfc9305ea75ee8
Rahexx <endriu1297@gmail.com>
2026-03-03 16:40:28 +0100

    feat: add tablet styles for FormHeader component

--- components/Form/FormHeader.tsx
+++ components/Form/FormHeader.tsx
@@ -8,7 +8,7 @@ interface FormHeaderProps {
 function FormHeader({ title, description }: FormHeaderProps){
     return (
         <header className="flex flex-col gap-2">
-            <h1 className="text-blue-950 font-bold leading-[1.2] text-2xl">
+            <h1 className="text-blue-950 font-bold leading-[1.2] text-2xl md:text-[2rem]">
                 {title}
             </h1>
             <p className="text-gray-500 text-base leading-[1.5]">

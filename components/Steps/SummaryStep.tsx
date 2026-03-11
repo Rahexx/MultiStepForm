23afcee5c4229de53322c275b0f105c05186c05b
Rahexx <endriu1297@gmail.com>
2026-03-11 16:59:30 +0100

    feat: add tablet styles for SummaryStep

--- components/Steps/SummaryStep.tsx
+++ components/Steps/SummaryStep.tsx
@@ -13,14 +13,14 @@ function SummaryStep() {
     const text = "text-sm leading-[120%]";
 
     return (
-        <div className="flex flex-col gap-y-6">
+        <div className="flex flex-col gap-y-6 md:gap-y-8">
             <section className="flex flex-col p-4 bg-blue-50 rounded-lg">
                 <div className="flex justify-between items-center mb-4">
                     <div className="flex flex-col">
-                        <h3 className="text-blue-950 text-sm font-medium leading-normal">{activePlan} ({isYearlyCharge ? "Yearly" : "Monthly"})</h3>
-                        <p className={`${text} text-gray-500 hover:cursor-pointer`}>Change</p>
+                        <h3 className="text-blue-950 text-sm font-medium leading-normal md:text-base md:leading-[120%]">{activePlan} ({isYearlyCharge ? "Yearly" : "Monthly"})</h3>
+                        <p className={`${text} text-gray-500 hover:cursor-pointer md:mt-2`}>Change</p>
                     </div>
-                    <p className={`${text} text-blue-950 font-bold tracking-[0.063rem]`}>$9/{isYearlyCharge ? "yr" : "mo"}</p>
+                    <p className={`${text} text-blue-950 font-bold tracking-[0.063rem] md:text-base md:tracking-[0rem]`}>$9/{isYearlyCharge ? "yr" : "mo"}</p>
                 </div>
                 <span className="block h-px w-full bg-gray-500 opacity-20"></span>
                 <ul className="flex flex-col gap-y-4 mt-4">
@@ -32,9 +32,9 @@ function SummaryStep() {
                     ))}
                 </ul>
             </section>
-            <section className="flex justify-between items-center mx-6">
+            <section className="flex justify-between items-center mx-6 md:mx-[1.406rem]">
                 <p className={`${text} text-gray-500`}>Total (per {isYearlyCharge ? "year" : "month"})</p>
-                <p className="text-base text-purple-600 font-bold leading-tight">${total}/{isYearlyCharge ? "yr" : "mo"}</p>
+                <p className="text-base text-purple-600 font-bold leading-tight md:text-xl">${total}/{isYearlyCharge ? "yr" : "mo"}</p>
             </section>
         </div>
     )

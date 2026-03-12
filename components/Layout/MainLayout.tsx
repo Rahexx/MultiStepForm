42f60be226d1ff347ccb62e0e2b74b8fdbbe0ee7
Rahexx <endriu1297@gmail.com>
2026-03-12 18:47:07 +0100

    feat: add desktop styles for MainLayout and StepsLayout components

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 function MainLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="relativeh-screen flex flex-col justify-between md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:h-[37.5rem] md:w-[45.75rem] md:bg-white md:rounded-2xl md:shadow-[9px_25px_40px_-20px_rgba(0,0,0,0.0951)] md:overflow-hidden md:p-3">
+        <div className="relativeh-screen flex flex-col justify-between md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:h-[37.5rem] md:w-[45.75rem] md:bg-white md:rounded-2xl md:shadow-[9px_25px_40px_-20px_rgba(0,0,0,0.0951)] md:overflow-hidden md:p-3 xl:w-[58.75rem]">
             {children}
         </div>
     );

707ce49c6387c27751188cfdad452f7747af7422
Rahexx <endriu1297@gmail.com>
2026-03-16 16:51:48 +0100

    feat: add desktop styles FormLayout and FooterLayout

--- components/Layout/FooterLayout.tsx
+++ components/Layout/FooterLayout.tsx
@@ -1,6 +1,6 @@
 function FooterLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="h-[4.5rem] flex items-center justify-center bg-white md:absolute md:bottom-8 md:right-[2.375rem] md:bg-transparent">
+        <div className="h-[4.5rem] flex items-center justify-center bg-white md:absolute md:bottom-8 md:right-[2.375rem] md:bg-transparent lg:bottom-12 lg:right-[6.25rem]">
             <div className="w-[21.438rem] flex items-center justify-between md:w-[21.75rem]">
                 {children}
             </div>
--- components/Layout/FormLayout.tsx
+++ components/Layout/FormLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FormLayout({ children }: { children: React.ReactNode }){
     return (
-        <section className="absolute top-[4.6875rem] left-1/2 -translate-x-1/2 w-[21.438rem] py-8 px-6 flex flex-col gap-6 bg-white rounded-[0.625rem] md:relative md:w-[21.75rem] md:top-0 md:left-0 md:translate-x-0 md:bg-transparent md:p-0 md:pt-[1.938rem]">
+        <section className="absolute top-[4.6875rem] left-1/2 -translate-x-1/2 w-[21.438rem] py-8 px-6 flex flex-col gap-6 bg-white rounded-[0.625rem] md:relative md:w-[21.75rem] md:top-0 md:left-0 md:translate-x-0 md:bg-transparent md:p-0 md:pt-[1.938rem] lg:w-[28.125rem] lg:pr-[5.5rem]">
             {children}
         </section>
     );

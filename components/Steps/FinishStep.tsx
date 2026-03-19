fe3eeeb9f8ba2359603066da057d1d3aa3fab139
Rahexx <endriu1297@gmail.com>
2026-03-19 16:23:05 +0100

    feat: add desktop styles for FInishStep

--- components/Steps/FinishStep.tsx
+++ components/Steps/FinishStep.tsx
@@ -7,7 +7,7 @@ function FinishStep() {
                 <Image src="/images/icon-thank-you.svg" alt="Thank you" fill />
             </div>
             <h3 className="block mt-6 mb-2 text-blue-950 text-2xl text-center font-bold leading-[120%] md:text-[2rem] md:mt-8 md:mb-4">Thank you!</h3>
-            <p className="text-gray-500 text-base leading-normal text-center md:w-[19.688rem] md:mx-auto">
+            <p className="text-gray-500 text-base leading-normal text-center md:w-[19.688rem] md:mx-auto lg:w-[28.125rem]">
                 Thanks for confirming your subscription! We hope you have fun 
                 using our platform. If you ever need support, please feel free 
                 to email us at support@loremgaming.com.
